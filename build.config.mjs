7228e06401cfe8b8408df7f721a0ac2e9ade624f
Pooya Parsa <pooya@pi0.io>
2025-09-19 10:05:58 +0200

    build: fix pkg injection

--- build.config.mjs
+++ build.config.mjs
@@ -1,13 +1,24 @@
 import { defineBuildConfig } from "obuild/config";
 import pkg from "./package.json" with { type: "json" };
 
+console.log({
+  "globalThis.__pkg_version__": JSON.stringify(pkg.version),
+  "globalThis.__pkg_name__": JSON.stringify(pkg.name),
+  "globalThis.__pkg_description__": JSON.stringify(pkg.description),
+});
+
 export default defineBuildConfig({
-  entries: ["./src/cli.ts"],
-  rolldown: {
-    define: {
-      "globalThis.__pkg_version__": JSON.stringify(pkg.version),
-      "globalThis.__pkg_name__": JSON.stringify(pkg.name),
-      "globalThis.__pkg_description__": JSON.stringify(pkg.description),
+  entries: [
+    {
+      type: "bundle",
+      input: "./src/cli.ts",
+      rolldown: {
+        define: {
+          "globalThis.__pkg_version__": JSON.stringify(pkg.version),
+          "globalThis.__pkg_name__": JSON.stringify(pkg.name),
+          "globalThis.__pkg_description__": JSON.stringify(pkg.description),
+        },
+      },
     },
-  },
+  ],
 });
